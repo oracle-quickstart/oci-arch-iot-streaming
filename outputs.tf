2589e620aeb0a3fde8e3e18fe21688eae44c2011
lfeldman <lukasz.feldman@oracle.com>
2020-11-16 13:28:03 +0100

    Support for ORM Deploy button v7

--- outputs.tf
+++ outputs.tf
@@ -16,3 +16,7 @@ output "oci_services" {
 output "service_gateways" {
   value = [data.oci_core_service_gateways.sgws.service_gateways]
 }
+
+output "generated_ssh_private_key" {
+  value = tls_private_key.public_private_key_pair.private_key_pem
+}

c7a687270c6fe3988ea8fff76a6579f6264e5598
lfeldman <lukasz.feldman@oracle.com>
2020-11-23 13:13:30 +0100

    Fixes on private network

--- network.tf
+++ network.tf
@@ -37,6 +37,12 @@ resource "oci_core_service_gateway" "sgw" {
   route_table_id = oci_core_route_table.rt_transit_routing_sgw.id
 }
 
+resource "oci_core_nat_gateway" "natgw" {
+    compartment_id = var.compartment_ocid
+    display_name = "nat_gateway"
+    vcn_id = oci_core_virtual_network.vcn.id
+}
+
 resource "oci_core_route_table" "rt_via_sgw" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_virtual_network.vcn.id
@@ -60,6 +66,16 @@ resource "oci_core_route_table" "rt_via_igw" {
     }
 }
 
+resource "oci_core_route_table" "rt_via_natgw" {
+    compartment_id = var.compartment_ocid
+    vcn_id         = oci_core_virtual_network.vcn.id
+    display_name   = "rt_via_natgw"
+    route_rules {
+        destination = "0.0.0.0/0"
+        destination_type  = "CIDR_BLOCK"
+        network_entity_id = oci_core_nat_gateway.natgw.id
+    }
+}
 
 resource "oci_core_security_list" "seclist1" {
     compartment_id = var.compartment_ocid
@@ -124,7 +140,7 @@ resource "oci_core_subnet" "ATPsubnet" {
   dns_label                  = "sub2"
   compartment_id             = var.compartment_ocid
   vcn_id                     = oci_core_virtual_network.vcn.id
-  route_table_id             = oci_core_route_table.rt_via_igw.id
+  route_table_id             = oci_core_route_table.rt_via_natgw.id
   dhcp_options_id            = oci_core_dhcp_options.dhcpoptions1.id
   prohibit_public_ip_on_vnic = true
 }

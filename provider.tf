a4f1b645ffadc7d412e4d7992d7f50ca5d3a1454
lfeldman <lukasz.feldman@oracle.com>
2021-01-13 14:06:26 +0100

    Fixes resource-manager/oci-arch-iot-streaming.zip

--- provider.tf
+++ provider.tf
@@ -1,12 +1,8 @@
 ## Copyright (c) 2020, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-terraform {
-  required_version = "~> 0.12"
-}
-
 provider "oci" {
-  version          = "= 3.76.0"
+  version          = ">= 3.76.0"
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
